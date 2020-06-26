e07ad1dcd9293c9182a53a5c2f6ef6011c4dd8eb
Sergio Medialdea <sergio.medialdea@twtspain.com>
2020-06-26 17:53:48 +0200

    Removed unused imports

--- src/app/features/todo-list/todo-list.module.ts
+++ src/app/features/todo-list/todo-list.module.ts
@@ -4,8 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { TodoListContainerComponent } from './containers/todo-list-container/todo-list-container.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoService } from './services/todo.service';
-import { GridModule } from '@progress/kendo-angular-grid';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
